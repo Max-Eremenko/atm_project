3d6cd140dd8ce57ef15e51bed645da50a0dc6b2e
Max <emndems216@gmail.com>
2020-06-14 19:22:46 +0300

    added new method

--- tests_atm.py
+++ tests_atm.py
@@ -8,17 +8,39 @@ class TestATM(unittest.TestCase):
         self.balance = 10000
         self.attempts = 2
         self.pin = 777
-        self.rise_money = 1
+        self.rise_money = 0
 
     def test_rise_money(self):
-        self.assertTrue(self.set.rise_money(rise_money=1), True)
+        self.assertTrue(self.set.rise_money(rise_money=0))
 
     def test_pin(self):
-        self.assertTrue(self.set.enter_pin(777), self.pin)
+        self.assertTrue(self.set.enter_pin(777))
 
     def test_pin2(self):
-        with self.assertRaises(IncorrectPin, msg="Incorrect Pin!!!"):
+        self.assertTrue(AttemptsOver, self.set.enter_pin(pin=777))
+
+    def test_pin3(self):
+        self.assertTrue(IncorrectPin, self.set.enter_pin(pin=777))
+
+    # def test_pin4(self):
+    #     self.assertTrue(self.set.enter_pin(client_can_get_money='get'), True)
+
+    def test_pin_negative(self):
+        self.assertIsNot(self.set.enter_pin(pin=777), self.pin, msg="Check Pin")
+
+    def test_pin_negative1(self):
+        self.assertRaises(AttemptsOver, msg="Attempts are over!!!")
+        self.set.enter_pin(pin=777)
+
+    def test_pin_negative2(self):
+        with self.assertRaises(IncorrectPin, msg="Check Incorrect Pin!!!"):
             self.set.enter_pin(0)
+
+
+
+
+
+
 
 
 
