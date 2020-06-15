311adc710b34ef2b57e803467636831024e07644
Max <emndems216@gmail.com>
2020-06-15 15:08:18 +0300

    correction method

--- tests_atm.py
+++ tests_atm.py
@@ -9,6 +9,7 @@ class TestATM(unittest.TestCase):
         self.attempts = 2
         self.pin = 777
         self.rise_money = 0
+        # self.client = False
 
     def test_rise_money(self):
         self.assertTrue(self.set.rise_money(rise_money=0))
@@ -23,7 +24,7 @@ class TestATM(unittest.TestCase):
         self.assertTrue(IncorrectPin, self.set.enter_pin(pin=777))
 
     # def test_pin4(self):
-    #     self.assertTrue(self.set.enter_pin(client_can_get_money='get'), True)
+    #     self.assertTrue(self.set.enter_pin(pin=777), self.client)
 
     def test_pin_negative(self):
         self.assertIsNot(self.set.enter_pin(pin=777), self.pin, msg="Check Pin")
@@ -33,8 +34,9 @@ class TestATM(unittest.TestCase):
         self.set.enter_pin(pin=777)
 
     def test_pin_negative2(self):
-        with self.assertRaises(IncorrectPin, msg="Check Incorrect Pin!!!"):
-            self.set.enter_pin(0)
+        self.assertRaises(IncorrectPin, msg="Check Incorrect Pin!!!")
+        self.set.enter_pin(pin=777)
+
 
 
 
