f80507a1fe63f2f1b7a9d0f7363a6625ab53e686
Max <emndems216@gmail.com>
2020-06-16 21:55:38 +0300

    new methods

--- tests_atm.py
+++ tests_atm.py
@@ -9,39 +9,54 @@ class TestATM(unittest.TestCase):
         self.attempts = 2
         self.pin = 777
 
-    def test_replenish_balance(self):
+    def test_replenish(self):
         self.assertEqual(self.set.rise_money(rise_money=3000), 13000)
 
-    def test_withdraw_money(self):
-        self.assertNotEqual(self.set.rise_money(rise_money=1000), 9000)
+    def test_replenishing_without_change_balance(self):
+        self.assertNotEqual(self.set.rise_money(rise_money=2000), 10000)
 
     def test_correct_pin(self):
         self.assertTrue(self.set.enter_pin(self.pin))
 
-    def test_attempts_over(self):
-        self.assertTrue(AttemptsOver, self.set.enter_pin(self.pin))
-
     def test_incorrect_pin(self):
-        self.assertTrue(IncorrectPin, self.set.enter_pin(self.pin))
+        self.assertNotEqual(self.set.enter_pin(self.pin), 555)
+        self.assertRaises(IncorrectPin)
 
-    def test_attempts_condition(self):
-        self.assertRaises(AttemptsOver, msg="Attempts are over!!!")
+    def test_attempts(self):
+        self.assertNotEqual(self.set.enter_pin(self.pin), 444)
+        self.attempts = self.attempts - 1
+        self.assertRaises(IncorrectPin)
+        self.assertNotEqual(self.set.enter_pin(self.pin), 333)
+        self.attempts = self.attempts - 0
+        self.assertRaises(AttemptsOver)
+
+    def test_withdraw_money(self):
         self.set.enter_pin(self.pin)
+        self.assertNotEqual(self.set.get_money(10000), self.balance-5000)
+        self.balance = 5000
 
-    def test_incorrect_condition(self):
-        self.assertRaises(IncorrectPin, msg="Check Incorrect Pin!!!")
-        self.set.enter_pin(self.pin)
-
-    def test_enter_pin_condition(self):
-        self.assertRaises(EnterPin, msg="Enter pin first!!!")
-        self.set.rise_money(self.pin)
-
-    def test_atm_balance_condition(self):
-        self.assertRaises(AtmBalance, msg="Atm balance is no enough!!!")
-        self.set.rise_money(self.pin)
-
-    def test_check_balance(self):
+    def test_correct_balance(self):
         self.assertEqual(self.balance, 10000)
+
+    def test_incorrect_balance(self):
+        self.assertNotEqual(self.balance, 50000)
+
+    # def test_attempts_condition(self):
+    #     self.assertRaises(AttemptsOver, msg="Attempts are over!!!")
+    #     self.set.enter_pin(self.pin)
+    #
+    # def test_incorrect_condition(self):
+    #     self.assertRaises(IncorrectPin, msg="Check Incorrect Pin!!!")
+    #     self.set.enter_pin(self.pin)
+    #
+    # def test_enter_pin_condition(self):
+    #     self.assertRaises(EnterPin, msg="Enter pin first!!!")
+    #     self.set.enter_pin(self.pin)
+    #
+    # def test_atm_balance_condition(self):
+    #     self.assertRaises(AtmBalance, msg="Atm balance is no enough!!!")
+    #     self.set.enter_pin(self.pin)
+
 
 
 
